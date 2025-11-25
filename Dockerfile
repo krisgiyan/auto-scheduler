882209ce9d85b982384f3033052fd58e16798208
krisgiyan <ikristiyan@outlook.com>
2025-11-25 18:21:37 +0000

    changes for Cloud Run config

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ RUN npx playwright install --with-deps
 
 COPY . .
 
-EXPOSE 4000
+# EXPOSE 4000
 CMD ["npm", "run" "start"]
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use('/scrape', scrape);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
